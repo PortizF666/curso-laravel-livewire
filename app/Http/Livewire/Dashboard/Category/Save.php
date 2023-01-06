b454ca8a82c72e7c46443b1f54c989d13dd4fffd
Priscila <pzitros90@gmail.com>
2023-01-06 13:24:41 -0600

    Creacion de validaciones

--- app/Http/Livewire/Dashboard/Category/Save.php
+++ app/Http/Livewire/Dashboard/Category/Save.php
@@ -6,8 +6,27 @@ use Livewire\Component;
 
 class Save extends Component
 {
+    public $title;
+    public $text;
+
+    protected $rules=[
+      'title' => 'required|min:2|max:255',
+      'slug' => 'required|min:2|max:255',
+      'text' => 'nullable',
+    ];
     public function render()
     {
         return view('livewire.dashboard.category.save');
+    }
+    public function submit()
+    {
+        $this->validate();
+        Category::create(
+            [
+                'title' => $this->title,
+                'slug' => str($this->title)->slug(),
+                'text' => $this->text,
+            ]
+        );
     }
 }
--- resources/views/livewire/dashboard/category/save.blade.php
+++ resources/views/livewire/dashboard/category/save.blade.php
@@ -1,3 +1,21 @@
 <div>
-    {{-- Be like water. --}}
+    <form wire:submit.prevent="submit">
+        <label for=""/>Titulo</label>
+
+            @error('title')
+                {{ $message }}
+            @enderror
+
+        <input type="text" wire:model="title"/>
+
+        <label for=""/>Texto</label>
+
+            @error('text')
+                {{ $message }}
+            @enderror
+
+        <input type="text" wire:model="text"/>
+
+        <button type="submit">Enviar</button>
+    </form>
 </div>
